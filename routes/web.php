effee94cbf9ab77e76fee95755867fb242e68cd0
hidasyaqiib <hidaps2008@gmail.com>
2024-10-21 16:20:24 +0700

    HTML to CSS tailwind

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome'); //merujuk kehalaman welcome.blade.php di folder resource/views
+});
+
+Route::get('/index', function () {
+    return view('index'); //merujuk kehalaman welcome.blade.php di folder resource/views
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
